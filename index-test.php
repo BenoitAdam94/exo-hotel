51724b593c83d67143bb6627b75b1185dcc94f71
BenoitAdam94 <adam050986@yahoo.fr>
2020-01-22 12:12:13 +0100

    indexphp corrigé

--- index-test.php
+++ index-test.php
@@ -3,172 +3,171 @@
 include 'aa-php-header-menu.htm';
 
 ?>
-
-<div class="row fondecran d-none .d-sm-flex d-sm-none d-md-flex">
-    <div class="col-sm-12 p-0">
-        <!-- ******************** -->
-        <!-- CARROUSSEL BOOTSTRAP -->
-        <!-- ******************** -->
-        <div id="carouselExampleIndicators" class="carousel slide carousel-faddata-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block" src="img/hotel/accueil-hotel.jpg" alt="Firslide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block" src="img/chambres/suichambre-resize-accueil.jpg" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block" src="img/hotel/fenetre.jpg" alt="Third slide">
-              </div>
+        <div class="row fondecran d-none .d-sm-flex d-sm-none d-md-flex">
+            <div class="col-sm-12 p-0">
+                <!-- ******************** -->
+                <!-- CARROUSSEL BOOTSTRAP -->
+                <!-- ******************** -->
+                <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block" src="img/hotel/accueil-hotel.jpg" alt="First slide">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block" src="img/chambres/suite/chambre-resize-accueil.jpg" alt="Second slide">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block" src="img/hotel/fenetre.jpg" alt="Third slide">
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+                  <div class="card-img-overlay d-flex justify-content-center align-items-end text-center text-accueil text-shadow parisienne ensavoirplus-position">
+                    <a href="#destination1">
+                        <h2 class="text-shadow">en savoir plus</h2>
+                        <span class="carousel-control-next-icon r90"></span>
+                    </a>
+                  
+                  
+                </div>    
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicatorrole="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicatorrole="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-          <div class="card-img-overlay d-flex justify-content-center align-items-etext-center text-accueil text-shadow parisienne ensavoirplus-position">
-            <a href="#destination1">
-                <h2 class="text-shadow">en savoir plus</h2>
-                <span class="carousel-control-next-icon r90"></span>
-            </a>
-          
-          
-        </div>    
-    </div>
-    
-</div>
-<!-- ********** -->
-<!-- 3 boutons -->
-<!-- ********** -->
-<div class="row boutons" id="destination1">
-    
-    <div class="col-sm-4 d-flex justify-content-center align-items-center p-4">
-        <a href="chambres-et-gammes.htm">
-        <figure class="c4-izmir c4-border-corners-1" style="--primary-color: #FFFF --image-opacity: .8">
             
-            <img src="img/chambres/suite/chambre-suite-new.jpg" alt="ChambrHôtelCop">
+        </div>
+        <!-- ********** -->
+        <!-- 3 boutons -->
+        <!-- ********** -->
+        <div class="row boutons" id="destination1">
+            
+            <div class="col-sm-4 d-flex justify-content-center align-items-center p-4">
+                <a href="chambres-et-gammes.htm">
+                <figure class="c4-izmir c4-border-corners-1" style="--primary-color: #FFFFFF; --image-opacity: .8">
+                    
+                    <img src="img/chambres/suite/chambre-suite-new.jpg" alt="Chambres HôtelCop">
+                    
+                    
+                        <figcaption></figcaption>
+                </figure>                      
+                
+                <div class="card-img-overlay d-flex justify-content-start align-items-end text-legende overflow-hidden">
+                    <h2 class="parisienne text-shadow">Nos Chambres</h2>
+                </div>
+                </a>
+            </div>
             
             
-                <figcaption></figcaption>
-        </figure>                      
-        
-        <div class="card-img-overlay d-flex justify-content-start align-items-etext-legende overflow-hidden">
-            <h2 class="parisienne text-shadow">Nos Chambres</h2>
+            <div class="col-sm-4 d-flex justify-content-center align-items-center p-4">
+                <a href="restaurant-et-carte.htm">
+                <figure class="c4-izmir c4-border-corners-1" style="--primary-color: #FFFFFF; --image-opacity: .8">
+                    <img src="img/restaurant/table-resto.jpg" alt="Restaurant HôtelCop">
+                    <figcaption></figcaption>
+                    
+                </figure>                      
+                <div class="card-img-overlay d-flex justify-content-start align-items-end text-legende overflow-hidden">
+                    <h2 class="parisienne text-shadow">Notre Restaurant</hclass="parisienne>
+                </div>
+                </a>
+            </div>
+            <div class="col-sm-4 d-flex justify-content-center align-items-center p-4">
+                <a href="spa-soin.htm">
+                <figure class="c4-izmir c4-border-corners-1" style="--primary-color: #FFFFFF; --image-opacity: .8">
+                    <img src="img/spa/spa-accueil2.jpg" alt="Spa HôtelCop">
+                    <figcaption></figcaption>
+                    
+                </figure>
+                <div class="card-img-overlay d-flex justify-content-start align-items-end text-legende overflow-hidden">
+                    <h2 class="parisienne text-shadow">Le Spa et nos soins</h2>
+                </div>
+                </a>
+            </div>
         </div>
-        </a>
-    </div>
-
-    
-    <div class="col-sm-4 d-flex justify-content-center align-items-center p-4">
-        <a href="restaurant-et-carte.htm">
-        <figure class="c4-izmir c4-border-corners-1" style="--primary-color: #FFFF --image-opacity: .8">
-            <img src="img/restaurant/table-resto.jpg" alt="Restaurant HôtelCop">
-            <figcaption></figcaption>
-            
-        </figure>                      
-        <div class="card-img-overlay d-flex justify-content-start align-items-etext-legende overflow-hidden">
-            <h2 class="parisienne text-shadow">Notre Restaurant</hclass="parisienne>
+        <!-- ************************************************************ -->
+        <!-- Description de l'hotel, disparait sur smartphone et tablette -->
+        <!-- ************************************************************ -->
+        <div class="row description d-none d-sm-flex">
+            <div class="col-sm-7 d-flex align-items-center p-1 overflow-hidden">
+                <img class="rounded" src="img/quartier/façade-resize.jpg" alt="Façade HôtelCop">
+            </div>
+            <article class="col-sm-5">
+                <br>
+                <h3>Une adresse capitale</h3>
+                <p>Ce nouvel hôtel (très) particulier possède l'élégance d'un appartement privé du XIXe siècle, où les services ultra personnalisés se vivent en toute simplicité et discrétion.<br><br>
+                Partout, les volumes immenses sont baignés de lumière, à peine tamisée par les riches étoffes des tentures. Le bonheur des grands espaces intérieurs, typiques des appartements haussmanniens... Les matières sont nobles et précieuses, une générosité oubliée. <br><br> Un restaurant étoilé, un luxueux spa et une cour intérieure verdoyante qui fait écho à l'entrée paysagère... Chez <em>HôtelCop</em> vous (re)vivez la quintessence du chic parisien de la Belle Epoque.</p>
+            </article>
+            <article class="col-sm-5">
+                <br>
+                <h3>Au coeur de l'ouest parisien</h3>
+                <br>
+                <p>A deux pas des Champs-Elysées, là, palpite le cœur de Paris. Lovée au calme, en retrait de la plus belle avenue du monde, <em>HôtelCop</em> a vue sur le Grand Palais, la Tour Eiffel, le Panthéon et l'obélisque de la Concorde. Juste devant, un rideau d'arbres majestueux préserve l'intimité des lieux, comme un écho aux Jardins de l'Elysée tout proche.</p>
+            </article>
+            <div class="col-sm-7 d-flex align-items-center p-1 rounded overflow-hidden">
+                <img class="rounded" src="img/hotel/exterieur-resize.jpg" alt="Façade HôtelCop">
+            </div>
+            <div class="col-sm-7 d-flex align-items-center p-1 rounded overflow-hidden">
+                <img class="rounded" src="img/hotel/event-resize.jpg" alt="Jardin HôtelCop">
+            </div>
+            <article class="col-sm-5">
+                <br>
+                <h3>Pour célébrer les événements de la vie avec style et élégance</h3>
+                <br>
+                <p>Depuis plus d’un siècle nous avons le privilège d’organiser des évènements d’exception au cœur de notre hôtel. Du plus simple au plus majestueux, du plus intime au plus extravagant, nous faisons de chaque évènement un évènement unique. C’est pour cela que notre équipe se consacre entièrement à parfaire chaque détail afin de vous offrir un moment inoubliable. Rien ne nous inspire plus que d’organiser les plus heureuses des célébrations.</p>
+            </article>     
         </div>
-        </a>
-    </div>
-    <div class="col-sm-4 d-flex justify-content-center align-items-center p-4">
-        <a href="spa-soin.htm">
-        <figure class="c4-izmir c4-border-corners-1" style="--primary-color: #FFFF --image-opacity: .8">
-            <img src="img/spa/spa-accueil2.jpg" alt="Spa HôtelCop">
-            <figcaption></figcaption>
-            
-        </figure>
-        <div class="card-img-overlay d-flex justify-content-start align-items-etext-legende overflow-hidden">
-            <h2 class="parisienne text-shadow">Le Spa et nos soins</h2>
+        <!-- ********** -->
+        <!-- Newsletter -->
+        <!-- ********** -->
+        <div class="row newsletter justify-content-center">
+            <div class="text-center p-3">
+                <div class="card h-100 p-3">
+                    <br><h3 class="parisienne">Newsletter</h3>
+                    <i class="fas fa-envelope-open-text fa-2x p-4"></i>
+                    <p>Abonnez-vous à notre newsletter et recevez les news & offres exclusives.</p>
+                    <form>
+                        <input id="newsletter" name="newsletter" type="email" value="Votre adresse Email"><button id="inscription-newsletter">S'inscrire</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        </a>
-    </div>
-</div>
-<!-- ************************************************************ -->
-<!-- Description de l'hotel, disparait sur smartphone et tablette -->
-<!-- ************************************************************ -->
-<div class="row description d-none d-sm-flex">
-    <div class="col-sm-7 d-flex align-items-center p-1 overflow-hidden">
-        <img class="rounded" src="img/quartier/façade-resize.jpg" alt="FaçaHôtelCop">
-    </div>
-    <article class="col-sm-5">
-        <br>
-        <h3>Une adresse capitale</h3>
-        <p>Ce nouvel hôtel (très) particulier possède l'élégance d'un appartemeprivé du XIXe siècle, où les services ultra personnalisés se vivent en tousimplicité et discrétion.<br><br>
-        Partout, les volumes immenses sont baignés de lumière, à peine tamisée ples riches étoffes des tentures. Le bonheur des grands espaces intérieurtypiques des appartements haussmanniens... Les matières sont nobles précieuses, une générosité oubliée. <br><br> Un restaurant étoilé, luxueux spa et une cour intérieure verdoyante qui fait écho à l'entrpaysagère... Chez <em>HôtelCop</em> vous (re)vivez la quintessence du chparisien de la Belle Epoque.</p>
-    </article>
-    <article class="col-sm-5">
-        <br>
-        <h3>Au coeur de l'ouest parisien</h3>
-        <br>
-        <p>A deux pas des Champs-Elysées, là, palpite le cœur de Paris. Lovée calme, en retrait de la plus belle avenue du monde, <em>HôtelCop</em> a vsur le Grand Palais, la Tour Eiffel, le Panthéon et l'obélisque de Concorde. Juste devant, un rideau d'arbres majestueux préserve l'intimides lieux, comme un écho aux Jardins de l'Elysée tout proche.</p>
-    </article>
-    <div class="col-sm-7 d-flex align-items-center p-1 rounded overflow-hidden">
-        <img class="rounded" src="img/hotel/exterieur-resize.jpg" alt="FaçaHôtelCop">
-    </div>
-    <div class="col-sm-7 d-flex align-items-center p-1 rounded overflow-hidden">
-        <img class="rounded" src="img/hotel/event-resize.jpg" alt="Jardin HôtelCop">
-    </div>
-    <article class="col-sm-5">
-        <br>
-        <h3>Pour célébrer les événements de la vie avec style et élégance</h3>
-        <br>
-        <p>Depuis plus d’un siècle nous avons le privilège d’organiser dévènements d’exception au cœur de notre hôtel. Du plus simple au plmajestueux, du plus intime au plus extravagant, nous faisons de chaqévènement un évènement unique. C’est pour cela que notre équipe se consacentièrement à parfaire chaque détail afin de vous offrir un momeinoubliable. Rien ne nous inspire plus que d’organiser les plus heureusdes célébrations.</p>
-    </article>     
-</div>
-<!-- ********** -->
-<!-- Newsletter -->
-<!-- ********** -->
-<div class="row newsletter justify-content-center">
-    <div class="text-center p-3">
-        <div class="card h-100 p-3">
-            <br><h3 class="parisienne">Newsletter</h3>
-            <i class="fas fa-envelope-open-text fa-2x p-4"></i>
-            <p>Abonnez-vous à notre newsletter et recevez les news & offrexclusives.</p>
-            <form>
-                <input id="newsletter" name="newsletter" type="email" value="Votadresse Email"><button id="inscription-newsletter">S'inscrirbutton>
-            </form>
+        <!-- ********* -->
+        <!-- 3 boutons -->
+        <!-- ********* -->
+        <div class="row boutons-bas">
+            <div class="col-sm-4 text-center p-3">
+                <div class="card h-100 p-3">
+                    <a href="avis.htm">
+                    <i class="fas fa-star fa-2x p-4"></i>
+                    <br><h3 class="parisienne">Avis</h3>
+                    <p>Ce que nos clients pensent de nous</p></a>
+                </div>
+            </div>
+            <div class="col-sm-4 text-center p-3">
+                <div class="card h-100 p-3">
+                    <a href="hotel-evenements.htm">
+                    <i class="fas fa-glass-martini-alt fa-2x p-4"></i>
+                    <br><h3 class="parisienne">Evènements</h3>                    
+                    <p>Nos derniers évènements</p></a>
+                </div>
+            </div>
+            <div class="col-sm-4 text-center p-3">
+                <div class="card h-100 p-3">
+                    <a href="hotel-presse.htm">
+                    <i class="fas fa-newspaper fa-2x p-4"></i>
+                    <br><h3 class="parisienne">Presse</h3>                        
+                    <p>Lire nos dernières publications</p></a> 
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<!-- ********* -->
-<!-- 3 boutons -->
-<!-- ********* -->
-<div class="row boutons-bas">
-    <div class="col-sm-4 text-center p-3">
-        <div class="card h-100 p-3">
-            <a href="avis.htm">
-            <i class="fas fa-star fa-2x p-4"></i>
-            <br><h3 class="parisienne">Avis</h3>
-            <p>Ce que nos clients pensent de nous</p></a>
-        </div>
-    </div>
-    <div class="col-sm-4 text-center p-3">
-        <div class="card h-100 p-3">
-            <a href="hotel-evenements.htm">
-            <i class="fas fa-glass-martini-alt fa-2x p-4"></i>
-            <br><h3 class="parisienne">Evènements</h3>                    
-            <p>Nos derniers évènements</p></a>
-        </div>
-    </div>
-    <div class="col-sm-4 text-center p-3">
-        <div class="card h-100 p-3">
-            <a href="hotel-presse.htm">
-            <i class="fas fa-newspaper fa-2x p-4"></i>
-            <br><h3 class="parisienne">Presse</h3>                        
-            <p>Lire nos dernières publications</p></a> 
-        </div>
-    </div>
-</div>
 <?php
 
 include 'aa-php-footer.htm';
